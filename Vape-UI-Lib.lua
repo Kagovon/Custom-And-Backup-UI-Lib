3cecffcdfc909c37a69b575a7895defc175d477c
Kagovon <91791018+Kagovon@users.noreply.github.com>
2021-12-30 19:47:34 +0700

    Update Vape-UI-Lib.lua

--- Vape-UI-Lib.lua
+++ Vape-UI-Lib.lua
@@ -470,6 +470,7 @@ function lib:Window(text, preset, closebind)
 			
 	    if lol == true then
 		ButtonTitle.TextXAlignment = Enum.TextXAlignment.Center
+		ButtonTitle.Size = UDim2.new(1, 0, 1, 0)		
 	    else
 		ButtonTitle.TextXAlignment = Enum.TextXAlignment.Left
 	    end
