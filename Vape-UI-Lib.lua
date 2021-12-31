4852e3c6bf12f7d0bb12473cc3e4b7bbd2727faf
Kagovon <91791018+Kagovon@users.noreply.github.com>
2021-12-31 21:48:32 +0700

    omg sussy

--- Vape-UI-Lib.lua
+++ Vape-UI-Lib.lua
@@ -1,4 +1,11 @@
-local lib = {RainbowColorValue = 0, HueSelectionPosition = 0}
+local VapeName = "Seria" .. "-" .. "Hub" .. "-" .. "UwU"
+
+if game.CoreGui[VapeName] then
+	game.CoreGui[VapeName]:Destroy()
+	return
+end
+
+local lib = {RainbowColorValue = 1 HueSelectionPosition = 0}
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
@@ -8,30 +15,10 @@ local PresetColor = Color3.fromRGB(30,30,30)
 local CloseBind = Enum.KeyCode.RightControl
 local breh = {}
 
-local letters = {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "N", "M"} -- etc.
-local length = 18
-local text = ""
-local text2 = ""
-local text3 = ""
-
-for i = 1,length do
-	text = text.. "" ..letters[math.random(1,#letters)]
-end
-for i = 1,length do
-	text2 = text2.. "" ..letters[math.random(1,#letters)]
-end
-for i = 1,length do
-	text3 = text3.. "" ..letters[math.random(1,#letters)]
-end
-
-local VapeName = text .. "-" .. text2 .. "-" .. text3
-
 local ui = Instance.new("ScreenGui")
 ui.Name = VapeName
 ui.Parent = game.CoreGui
 ui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-
 
 
 coroutine.wrap(
